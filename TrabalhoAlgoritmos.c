01bb16e43687812117a9954ce19a3b9ab215ef84
Leonardo Kuntz <99610937+scuiki@users.noreply.github.com>
2022-04-26 15:12:46 -0300

    Update TrabalhoAlgoritmos.c

--- TrabalhoAlgoritmos.c
+++ TrabalhoAlgoritmos.c
@@ -9,7 +9,7 @@
 int sport = 2, street = 3, yellow = 5, red = 7, lowCost = 11, mediumCost = 13, highCost = 17;
 
 //using "stdarg.h" in order to determine, when calling the function, how many arguments it contains
-int storeSorting (int tag,...) {
+int catalogueNumber (int tag,...) {
 
 va_list valist;
 
@@ -26,10 +26,10 @@ va_end(valist);
 	return catalogueNumber;
 }
 
-int tagSearching () {
+int searchNumber () {
 
 int i = 0, value[n], searchNumber = 1;
-char *sSport, *sStreet, *sYellow, *sRed, *sLowCost, *sMediumCost, *sHighCost, *sEnd = NULL;
+char *sSport, *sStreet, *sYellow, *sRed, *sLowCost, *sMediumCost, *sHighCost, *sEnd;
 
 	//executing while string doesn't contain "."
 	do {
@@ -82,13 +82,13 @@ int main () {
 	
 int store[s], srchNumber;
 
-	store[0] = storeSorting (3, sport, red, mediumCost);
-	store[1] = storeSorting (5, sport, red, mediumCost, street, yellow);
+	store[0] = catalogueNumber (3, sport, red, mediumCost);
+	store[1] = catalogueNumber (5, sport, red, mediumCost, street, yellow);
 	
 	printf("%d\n", store[0]);
 	printf("%d\n", store[1]);
 	
-	srchNumber = tagSearching ();
+	srchNumber = searchNumber ();
 	
 	printf("%d", srchNumber);	
 }
